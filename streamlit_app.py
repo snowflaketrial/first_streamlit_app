74a134ac150a6503f50008fdf3843dcf4a52d1df
snowflaketrial <116819282+snowflaketrial@users.noreply.github.com>
2022-10-27 11:46:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header("Breakfast menu")
+streamlit.text("Oatmeal")
+streamlit.text("Smoothie")
+streamlit.text("eggs")
